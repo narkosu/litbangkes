29451b53704faad0c31c7792ba47e8103b351189
budi sunarko <budisunarko@gmail.com>
2014-06-26 22:02:05 +0700

    delete files

--- themes/litbangkes/views/layouts/column1.php
+++
@@ -1,7 +0,0 @@
-<?php $this->beginContent('/layouts/main'); ?>
-<div class="container">
-	<div id="content" style="margin:15px;">
-		<?php echo $content; ?>
-	</div><!-- content -->
-</div>
-<?php $this->endContent(); ?>
