d789487bf33a86d9116e6123d182069295808ff1
budi sunarko <budisunarko@gmail.com>
2014-06-27 06:44:05 +0700

    Update Rename filename

--- protected/modules/penelitian/controllers/ProposalPenelitianController.php
+++ protected/modules/penelitian/controllers/ProposalPenelitianController.php
@@ -1,6 +1,6 @@
 <?php
 
-class ProposalPenelitianController extends Controller
+class ProposalpenelitianController extends Controller
 {
 	/**
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
