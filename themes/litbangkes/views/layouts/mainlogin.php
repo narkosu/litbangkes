69dcd549dcdd07927a1f698f03ef294b5e8c78e2
budi sunarko <budisunarko@gmail.com>
2014-07-24 02:22:02 +0700

    Change login page

--- themes/litbangkes/views/layouts/mainlogin.php
+++ themes/litbangkes/views/layouts/mainlogin.php
@@ -1,63 +1,53 @@
-<!DOCTYPE html>
+<!doctype html>
+<!--[if IE 7 ]>    <html lang="en-gb" class="isie ie7 oldie no-js"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en-gb" class="isie ie8 oldie no-js"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en-gb" class="isie ie9 no-js"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en-gb" class="no-js"> <!--<![endif]-->
+
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<title>SISTEM INFORMASI BADAN PENELITIAN LITBANGKES SURABAYA</title>
-<link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl .'/css/style.default.css'?>" type="text/css" />
-<?php /*<script type="text/javascript" src="<?php echo Yii::app()->theme->baseUrl .'/js/jquery-1.9.1.min.js'?>"></script>*/?>
-<script type="text/javascript" src="<?php echo Yii::app()->theme->baseUrl .'/js/jquery-migrate-1.1.1.min.js'?>"></script>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Login</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl .'/css/login.css'?>" />	
 </head>
 
-<body class="loginbody">
+<body>
 
-<div class="loginwrapper">
-	<div class="loginwrap zindex100 animate2 bounceInDown">
-	<h1 class="logintitle"><span class="iconfa-lock"></span> LOGIN FORM <span class="subtitle">Silahkan dengan username & password anda.</span></h1>
-        
-        <?php echo $content ?>
+	<div id="wrapper_login">
+		
+    <div id="left_login">
+    	<h1 class="login_h1">
+      	Selamat Datang di<br />
+        <span class="login_h2">Sistem Informasi Riset</span>
+      </h1>
+      
+      <h3>BADAN PENELITIAN DAN PENGEMBANGAN KESEHATAN</h3>
+      <?php echo $content ?>
+      <?php /*
+      <form name="login_form" action="#" method="post">
+      	<p>
+        	<input type="text" class="login_text" name="username" />
+        </p>
+        <p>
+        	<input type="password" class="login_text" name="password" />
+        </p>
+        <p>
+        	<input type="submit" value="Login" class="login_btn" />
+        </p>
+      </form>
+       * 
+       */
+      ?>
+    </div>  
+  	
+    <div id="right_login">
+    	<img src="<?php echo Yii::app()->theme->baseUrl ?>/img/iMac.png" alt="login sistem informasi riset"  />
     </div>
-</div><!--loginwrapper-->
-
-<script type="text/javascript">
-jQuery.noConflict();
-/*
-jQuery(document).ready(function(){
-	
-	var anievent = (jQuery.browser.webkit)? 'webkitAnimationEnd' : 'animationend';
-	jQuery('.loginwrap').bind(anievent,function(){
-		jQuery(this).removeClass('animate2 bounceInDown');
-	});
-	
-	jQuery('#username,#password').focus(function(){
-		if(jQuery(this).hasClass('error')) jQuery(this).removeClass('error');
-	});
-	
-	jQuery('#loginform button').click(function(){
-		if(!jQuery.browser.msie) {
-			if(jQuery('#username').val() == '' || jQuery('#password').val() == '') {
-				if(jQuery('#username').val() == '') jQuery('#username').addClass('error'); else jQuery('#username').removeClass('error');
-				if(jQuery('#password').val() == '') jQuery('#password').addClass('error'); else jQuery('#password').removeClass('error');
-				jQuery('.loginwrap').addClass('animate0 wobble').bind(anievent,function(){
-					jQuery(this).removeClass('animate0 wobble');
-				});
-			} else {
-				jQuery('.loginwrapper').addClass('animate0 fadeOutUp').bind(anievent,function(){
-					jQuery('#loginform').submit();
-				});
-			}
-			return false;
-		}
-	});
   
-});*/
-</script>
+  </div>
+  
+  <div id="login_footer">
+  	&copy; <?php echo date('Y');?> Pusat Humaniora, Kebijakan Kesehatan dan Pemberdayaan Masyarakat. All rights reserved.
+  </footer>
+  
 </body>
 </html>
-<?php
-$cs=Yii::app()->clientScript;
-
-$cs->registerScriptFile( Yii::app()->theme->baseUrl .'/js/jquery-1.9.1.min.js', CClientScript::POS_HEAD);
-$cs->scriptMap=array(
-    'jquery.js'=>'//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js',
-);
-?>
